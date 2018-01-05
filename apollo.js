624576cc3c0174c10c1b504510c7ee3767be3951
Striker <34070194+Strikerrr@users.noreply.github.com>
2018-01-05 19:27:14 +0000

    Added info

--- apollo.js
+++ apollo.js
@@ -1,1 +1,44 @@
-//Placeholder
+const Discord = require('discord.js');
+
+const client = new Discord.Client({
+
+disableEveryone: true
+
+});
+
+const pg = require('pg');
+
+const fs = require('fs');
+
+//Custom error catcher function
+
+function error(err) {
+
+const errorembed = new Discord.RichEmbed()
+.setColor(color)
+.setTitle('New Error Caught!')
+.setTimestamp()
+.setDescription(`\`\`\`xl\n${err.stack}\`\`\``);
+
+client.channels.get('385485532458778626').send({
+embed: errorembed
+});
+
+}
+
+//Event Handler
+
+fs.readdir('./events/', (err, files) => {
+if (err) return console.error(err);
+
+files.forEach(file => {
+const eventFunction = require(`./events/${file}`);
+const eventName = file.split('.')[0];
+client.on(eventName, (...args) => eventFunction.run(client, ...args));
+});
+});
+
+client.login(process.env.TOKEN);
+
+module.exports.error = error;
+

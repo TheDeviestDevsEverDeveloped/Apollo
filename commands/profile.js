e4fad98e00e239eef02a4d40bafc3fe4a7e0333e
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 09:39:45 -0500

    Update profile.js

--- commands/profile.js
+++ commands/profile.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, database) => {
         var mentionedID = message.author.id
         var mentionedUsername = message.author.username
     }
-    database.query('SELECT points FROM testusers WHERE userId = $1', [mentionedID], (err, res) => {
+    database.query('SELECT points FROM users WHERE userId = $1', [mentionedID], (err, res) => {
         if (err) {console.log(err); return}
         let points = res.rows[0];
         if(!points){message.channel.send('This user currently has no database stats')}

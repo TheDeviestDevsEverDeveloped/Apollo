ee5e15d4790799bcede0dd7c2b662b745acd8d16
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 08:09:10 -0500

    Add test bot

--- apollo.js
+++ apollo.js
@@ -42,3 +42,43 @@ client.login(process.env.TOKEN);
 
 module.exports.error = error;
 
+const Discord = require('discord.js');
+
+const testclient = new Discord.Client({
+
+disableEveryone: true
+
+});
+
+
+//Custom error catcher function
+
+function error(err) {
+
+const testerrorembed = new Discord.RichEmbed()
+.setColor(color)
+.setTitle('New Error Caught!')
+.setTimestamp()
+.setDescription(`\`\`\`xl\n${err.stack}\`\`\``);
+
+testclient.channels.get('385485532458778626').send({
+embed: testerrorembed
+});
+
+}
+
+//Event Handler
+
+fs.readdir('./events/', (err, files) => {
+if (err) return console.error(err);
+
+files.forEach(file => {
+const eventFunction = require(`./events/${file}`);
+const eventName = file.split('.')[0];
+testclient.on(eventName, (...args) => eventFunction.run(testclient, ...args));
+});
+});
+
+testclient.login(process.env.TESTTOKEN);
+
+module.exports.error = error;

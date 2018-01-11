992ad7902765543dff714cff2b1393756fbd6c65
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 10:09:34 -0500

    Update reset.js

--- testcommands/reset.js
+++ testcommands/reset.js
@@ -7,7 +7,7 @@ exports.run = (client, message, args, testdatabase) => {
     const mentionedAmount = args[1]
     if(!args[0]) {message.channel.send('You can\'t run this without any args')}
 
-    database.query('SELECT points FROM testusers WHERE userId = $1', [mentionedID], (err, res) => {
+    testdatabase.query('SELECT points FROM testusers WHERE userId = $1', [mentionedID], (err, res) => {
         if (err) {console.log(err); return}
         let points = res.rows[0];
         if(!points){message.channel.send('This user currently has no database stats')}

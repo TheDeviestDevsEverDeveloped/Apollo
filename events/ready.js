87dbb68097bf769537b6ab48a3dd13b3bbafae08
Mr.Table <Limegreenxd@gmail.com>
2018-01-11 04:50:32 -0500

    Create ready.js

---
+++ events/ready.js
@@ -0,0 +1,9 @@
+exports.run = (client) => {
+  client.user.setPresence({
+    game: {
+      name: `help | In ${client.guilds.size} servers!`,
+      type: 0
+    }
+  })
+  console.log(`Apollo is connected to the Discord WebSocket`)
+}

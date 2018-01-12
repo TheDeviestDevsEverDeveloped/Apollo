657b867f36b4ac900ff6a0ad1704d8e5fd77dd4c
Mr.Table <Limegreenxd@gmail.com>
2018-01-12 11:10:52 -0500

    Update leaderboards.js

--- testcommands/leaderboards.js
+++ testcommands/leaderboards.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args, testdatabase) => {
 testdatabase.query(`SELECT * FROM testusers ORDER BY userId`).then(rows => {
-                message.channel.send(rows[0] + "\n" + rows[1] + "\n" + rows.1);
+                message.channel.send(rows[0] + "\n" + rows[1] + "\n" + rows.userId);
         })   
 }

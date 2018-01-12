1f80932f46c053e7cc3a0834172e9c70b4a5a09f
Mr.Table <Limegreenxd@gmail.com>
2018-01-12 13:12:28 -0500

    Update leaderboards.js

--- testcommands/leaderboards.js
+++ testcommands/leaderboards.js
@@ -1,8 +1,9 @@
 exports.run = (client, message, args, testdatabase) => {
 testdatabase.query(`SELECT points FROM testusers`, (err, res) => {
         if (err) {console.log(err); return}
-        let points = res.rows
-points.sort(function(a, b){return b - a});
-message.channel.send(points[0])
+let array = [];
+res.rows.forEach(row => array.push(row.points));
+array = array.sort((a, b) => {return b - a});
+message.channel.send(array)
 });  
 }

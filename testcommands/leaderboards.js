dcd47b75316e68e3527b2afe68a8b20a62c219a1
Mr.Table <Limegreenxd@gmail.com>
2018-01-12 13:08:33 -0500

    Update leaderboards.js

--- testcommands/leaderboards.js
+++ testcommands/leaderboards.js
@@ -1,5 +1,5 @@
 exports.run = (client, message, args, testdatabase) => {
-testdatabase.query('SELECT points FROM testusers, (err, res) => {
+testdatabase.query(`SELECT points FROM testusers`, (err, res) => {
         if (err) {console.log(err); return}
         let points = res.rows
 points.sort(function(a, b){return b - a});

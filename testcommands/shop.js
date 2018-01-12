372be7c1adb0576074c4943747df8a1a1482804c
Mr.Table <Limegreenxd@gmail.com>
2018-01-12 08:43:58 -0500

    Create shop.js

---
+++ testcommands/shop.js
@@ -0,0 +1,16 @@
+exports.run = (client, message, args, testdatabase) => {
+    testdatabase.query('SELECT points FROM testusers WHERE userId = $1', [message.author.id], (err, res) => {
+        if (err) {console.log(err); return}
+        let points = res.rows[0];
+        if(!points){message.channel.send('This user currently has no database stats')}
+        else points = JSON.parse(res.rows[0].points);
+        let usrPoints = points.points
+        let usrLevel = points.level
+        let usrAwards = points.awards
+        let usrKills = points.zombiesSlain
+        let usrCoins = points.coins
+        let usrXP = points.xp
+        let coinsNeeded = (((usrLevel + 1) * 10)**2)
+message.channel.send(`**Shop Items:**\nID:1 XP boost level up \`Cost: ${points.xpBoostLevel * 100}\`\nID:2 Coin boost level up \`Cost: ${points.coinBoostLevel * 100}\`\nID:3 100 XP \`Cost: 500\`\nID:4 1500 XP \`Cost: 5000\`\nID:5 Reward level up \`Cost: 500\`\nID: 6 VIP Level 2 \`Cost: 50000\`\n\n**To buy an item, use r!buy <ID> <AMOUNT>**`)
+    });
+}

c30780a6aa75cdd2e82605a4bc668d4c8b9e82c8
Mr.Table <Limegreenxd@gmail.com>
2018-09-05 08:23:26 -0400

    Update apollo.js

--- apollo.js
+++ apollo.js
@@ -27,11 +27,29 @@ message.channel.send("!market buy " +(message.content.replace("mb ", "")))
       message.channel.send("!confirmbuy");
 }, 1000);
 	}
+	if(message.content === "mshowcmds"){
+message.channel.send("mshowcmds - *Shows this help list*\nmbing - *Checks if I'm online**\nmfind - *Searches pokecord market for pokemon*\nminfo - *Displays info on pokemon of mine*\nmpokes - *Used to search specific pokemon*\nmsopa - *Displays the cheapest pokemon on the pokecord market*\nmnext - *Sends '!next'*\nmback - *Sends '!back'*\nmpoke - *Shows my pokemon*\nmembed - *Embeds a message*")
+	}
 	if(message.content.startsWith("mfind ")){
 message.channel.send("!market search --name " + message.content.replace("mfind ", "") + " --showiv --order price a")
 	}
+	if(message.content.startsWith("minfo ")){
+message.channel.send("!info " + message.content.replace("minfo ", ""))
+	}
+	if(message.content.startsWith("mpokes ")){
+message.channel.send("!pokemon " + message.content.replace("mpokes ", ""))
+	}
 	if(message.content === "msopa"){
 message.channel.send("!market search --showiv --order price a")
+	}
+	if(message.content === "mpoke"){
+message.channel.send("!pokemon")
+	}
+	if(message.content === "mnext"){
+message.channel.send("!next")
+	}
+	if(message.content === "mback"){
+message.channel.send("!back")
 	}
 	if(message.content.startsWith("m!bing")){
     message.channel.sendEmbed({
